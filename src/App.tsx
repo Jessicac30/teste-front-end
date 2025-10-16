a78857567aa064cf12004c007bcd03eb2f7b97d5
Jessica Carvalho <jmaria.carvalho30@gmail.com>
2025-10-16 17:19:45 -0300

    feat: configuração inicial do projeto React + TypeScript + Vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Econverse - Vitrine de Produtos</h1>
+    </div>
+  );
+}
+
+export default App;
